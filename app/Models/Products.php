e220fb96f2d86fdc7323fbc5166d195b3a8600c2
BagasIbrahim <bagasibrahim9@gmail.com>
2023-05-27 17:39:55 +0700

    [FEAT] menambahkan model untuk tugas pertemuan 24

---
+++ app/Models/Products.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Products extends Model
+{
+    use HasFactory;
+    
+    protected $table = 'products';
+
+    protected $fillable = [
+        'category_id',
+        'name_product',
+        'description',
+        'price',
+        'status',
+        'created_by',
+        'verified_by',
+    ];
+
+    public function categories()
+    {
+        return $this->belongsTo(Categories::class, 'category_id' , 'id');
+    }
+
+    public function users()
+    {
+        return $this->belongsTo(Users::class, ['created_by', 'verified_by'], 'id');
+    }
+}
