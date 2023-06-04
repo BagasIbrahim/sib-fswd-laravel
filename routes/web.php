fdfd1ee9cc69732ca3e919bdb57e38be5019ee74
BagasIbrahim <bagasibrahim9@gmail.com>
2023-06-04 07:58:11 +0700

    [FIX] menambahkan route middleware sesuai role (tugas pertemuan 25)

--- routes/web.php
+++ routes/web.php
@@ -53,53 +53,59 @@ Route::get('/conflict/{nama}', function ($namaItem) {
     return "Item yang dicari adalah " . $namaItem;
 });
 
-
 //Route landing page
 Route::get('/', [LandingPageController::class, 'index']);
 
-//Route Dashboard
-Route::get('/dashboard', [DashboardController::class, 'index'])->middleware('auth');
+Route::middleware('role:admin')->group(function () {
+    // Route for CRUD Users
+    Route::get('/daftarpengguna/formtambah', [UserController::class, 'create'])->middleware('auth');
+    Route::post('/daftarpengguna/create', [UserController::class, 'store'])->middleware('auth');
+    Route::get('/daftarpengguna/detail/{id}', [UserController::class, 'show'])->middleware('auth');
+    Route::get('/daftarpengguna/formedit/{id}', [UserController::class, 'edit'])->middleware('auth');
+    Route::put('/daftarpengguna/update', [UserController::class, 'update'])->middleware('auth');
+    Route::get('/daftarpengguna/hapus/{id}', [UserController::class, 'destroy'])->middleware('auth');
 
-// Route for CRUD Users
-Route::get('/pengguna', [UserController::class, 'index']);
-Route::get('/daftarpengguna/formtambah', [UserController::class, 'create'])->middleware('auth');
-Route::post('/daftarpengguna/create', [UserController::class, 'store'])->middleware('auth');
-Route::get('/daftarpengguna/detail/{id}', [UserController::class, 'show'])->middleware('auth');
-Route::get('/daftarpengguna/formedit/{id}', [UserController::class, 'edit'])->middleware('auth');
-Route::put('/daftarpengguna/update', [UserController::class, 'update'])->middleware('auth');
-Route::get('/daftarpengguna/hapus/{id}', [UserController::class, 'destroy'])->middleware('auth');
+    // Route for CRUD Products
+    Route::get('/daftarproduk/formtambah', [ProductController::class, 'create'])->middleware('auth');
+    Route::post('/daftarproduk/create', [ProductController::class, 'store'])->middleware('auth');
+    Route::get('/daftarproduk/detail/{id}', [ProductController::class, 'show'])->middleware('auth');
+    Route::get('/daftarproduk/formedit/{id}', [ProductController::class, 'edit'])->middleware('auth');
+    Route::put('/daftarproduk/update', [ProductController::class, 'update'])->middleware('auth');
+    Route::get('/daftarproduk/hapus/{id}', [ProductController::class, 'destroy'])->middleware('auth');
 
-// Route for CRUD Products
-Route::get('/daftarproduk', [ProductController::class, 'index'])->middleware('auth');
-Route::get('/daftarproduk/formtambah', [ProductController::class, 'create'])->middleware('auth');
-Route::post('/daftarproduk/create', [ProductController::class, 'store'])->middleware('auth');
-Route::get('/daftarproduk/detail/{id}', [ProductController::class, 'show'])->middleware('auth');
-Route::get('/daftarproduk/formedit/{id}', [ProductController::class, 'edit'])->middleware('auth');
-Route::put('/daftarproduk/update', [ProductController::class, 'update'])->middleware('auth');
-Route::get('/daftarproduk/hapus/{id}', [ProductController::class, 'destroy'])->middleware('auth');
+    // Route for CRUD Categories
+    Route::get('/daftarkategori/formtambah', [CategoriesController::class, 'create'])->middleware('auth');
+    Route::post('/daftarkategori/create', [CategoriesController::class, 'store'])->middleware('auth');
+    Route::get('/daftarkategori/detail/{id}', [CategoriesController::class, 'show'])->middleware('auth');
+    Route::get('/daftarkategori/formedit/{id}', [CategoriesController::class, 'edit'])->middleware('auth');
+    Route::put('/daftarkategori/update', [CategoriesController::class, 'update'])->middleware('auth');
+    Route::get('/daftarkategori/hapus/{id}', [CategoriesController::class, 'destroy'])->middleware('auth');
 
-// Route for CRUD Categories
-Route::get('/daftarkategori', [CategoriesController::class, 'index'])->middleware('auth');
-Route::get('/daftarkategori/formtambah', [CategoriesController::class, 'create'])->middleware('auth');
-Route::post('/daftarkategori/create', [CategoriesController::class, 'store'])->middleware('auth');
-Route::get('/daftarkategori/detail/{id}', [CategoriesController::class, 'show'])->middleware('auth');
-Route::get('/daftarkategori/formedit/{id}', [CategoriesController::class, 'edit'])->middleware('auth');
-Route::put('/daftarkategori/update', [CategoriesController::class, 'update'])->middleware('auth');
-Route::get('/daftarkategori/hapus/{id}', [CategoriesController::class, 'destroy'])->middleware('auth');
+    // Route for CRUD Group Users
+    Route::get('/daftargruppengguna/formtambah', [GroupUserController::class, 'create'])->middleware('auth');
+    Route::post('/daftargruppengguna/create', [GroupUserController::class, 'store'])->middleware('auth');
+    Route::get('/daftargruppengguna/detail/{id}', [GroupUserController::class, 'show'])->middleware('auth');
+    Route::get('/daftargruppengguna/formedit/{id}', [GroupUserController::class, 'edit'])->middleware('auth');
+    Route::put('/daftargruppengguna/update', [GroupUserController::class, 'update'])->middleware('auth');
+    Route::get('/daftargruppengguna/hapus/{id}', [GroupUserController::class, 'destroy'])->middleware('auth');
+});
 
-// Route for CRUD Group Users
-Route::get('/daftargruppengguna', [GroupUserController::class, 'index'])->middleware('auth');
-Route::get('/daftargruppengguna/formtambah', [GroupUserController::class, 'create'])->middleware('auth');
-Route::post('/daftargruppengguna/create', [GroupUserController::class, 'store'])->middleware('auth');
-Route::get('/daftargruppengguna/detail/{id}', [GroupUserController::class, 'show'])->middleware('auth');
-Route::get('/daftargruppengguna/formedit/{id}', [GroupUserController::class, 'edit'])->middleware('auth');
-Route::put('/daftargruppengguna/update', [GroupUserController::class, 'update'])->middleware('auth');
-Route::get('/daftargruppengguna/hapus/{id}', [GroupUserController::class, 'destroy'])->middleware('auth');
+Route::middleware('role:admin,staff')->group(function () {
+    Route::get('/daftarpengguna', [UserController::class, 'index'])->middleware('auth');
+    Route::get('/daftarkategori', [CategoriesController::class, 'index'])->middleware('auth');
+    Route::get('/daftargruppengguna', [GroupUserController::class, 'index'])->middleware('auth');
+});
+
+
+Route::middleware('role:admin,staff,user')->group(function () {
+    Route::get('/dashboard', [DashboardController::class, 'index'])->middleware('auth');
+    Route::get('/daftarproduk', [ProductController::class, 'index'])->middleware('auth');
+});
 
 //Route login   
 Route::get('login', [LoginController::class, 'index']);
 Route::post('login', [LoginController::class, 'authenticate']);
 Route::post('logout', [LoginController::class, 'logout']);
-
+    
 //Route register
 Route::get('/register', [RegisterController::class, 'index']);
