3759c4638655fa72150585aeab0b1f8e656b1986
BagasIbrahim <bagasibrahim9@gmail.com>
2023-06-11 11:13:26 +0700

    [FIX] mengubah codingan dalam pemanggilan asset

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -12,7 +12,7 @@
     <title>Database Admin - Dashboard</title>
 
     <!-- Custom fonts for this template-->
-    <link href="{{asset('vendor/bootstrap/css/all.css')}}" rel="stylesheet" type="text/css">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.1/css/all.min.css" rel="stylesheet" type="text/css">
     <link
         href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
         rel="stylesheet">
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -15,7 +15,7 @@
 
     <!-- Load fonts style after rendering the layout styles -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;200;300;400;500;700;900&display=swap">
-    <link rel="stylesheet" href="{{asset('vendor/bootstrap/css/all.css')}}">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.1/css/all.min.css">
 
 </head>
 
