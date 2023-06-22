b65da3d01c9191d13516222dbc34aa64e195ac31
BagasIbrahim <bagasibrahim9@gmail.com>
2023-06-22 14:02:50 +0700

    [FIX] mengubah view untuk landing page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -20,33 +20,11 @@
 </head>
 
 <body>
-    <!-- Start Top Nav -->
-    <nav class="navbar navbar-expand-lg bg-dark navbar-light d-none d-lg-block" id="templatemo_nav_top">
-        <div class="container text-light">
-            <div class="w-100 d-flex justify-content-between">
-                <div>
-                    <i class="fa fa-envelope mx-2"></i>
-                    <a class="navbar-sm-brand text-light text-decoration-none" href="">mycart@company.com</a>
-                    <i class="fa fa-phone mx-2"></i>
-                    <a class="navbar-sm-brand text-light text-decoration-none" href="">0812384312</a>
-                </div>
-                <div>
-                    <a class="text-light" href="https://www.facebook.com/profile.php?id=100009399030321" target="_blank" rel="sponsored"><i class="fab fa-facebook-f fa-sm fa-fw me-2"></i></a>
-                    <a class="text-light" href="https://www.instagram.com/b_maulanaibrahim/" target="_blank"><i class="fab fa-instagram fa-sm fa-fw me-2"></i></a>
-                    <a class="text-light" href="https://github.com/BagasIbrahim" target="_blank"><i class="fab fa-github fa-sm fa-fw me-2"></i></a>
-                    <a class="text-light" href="https://www.linkedin.com/in/bagas-maulana-ibrahim/" target="_blank"><i class="fab fa-linkedin fa-sm fa-fw"></i></a>
-                </div>
-            </div>
-        </div>
-    </nav>
-    <!-- Close Top Nav -->
-
-
     <!-- Header -->
-    <nav class="navbar navbar-expand-lg navbar-light shadow">
+    <nav class="navbar fixed-top navbar-expand-lg navbar-light bg-light shadow">
         <div class="container d-flex justify-content-between align-items-center">
 
-            <a class="navbar-brand text-success logo h1 align-self-center" href="/">
+            <a class="navbar-brand text-success logo h2 align-self-center" href="/">
                 MyCart
             </a>
 
@@ -54,20 +32,17 @@
                 <span class="navbar-toggler-icon"></span>
             </button>
 
-            <div class="align-self-center collapse navbar-collapse flex-fill  d-lg-flex justify-content-lg-between" id="templatemo_main_nav">
+            <div class="align-self-center collapse navbar-collapse flex-fill d-lg-flex justify-content-lg-between" id="templatemo_main_nav">
                 <div class="flex-fill">
-                    <ul class="nav navbar-nav d-flex justify-content-between mx-lg-auto">
+                    <ul class="nav navbar-nav d-flex justify-content-start">
                         <li class="nav-item">
-                            <a class="nav-link" href="/">Home</a>
+                            <a class="nav-link ms-4=" href="/">Home</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="">About</a>
+                            <a class="nav-link ms-4" href="#product">Product</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="">Shop</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="">Contact</a>
+                            <a class="nav-link ms-4" href="#about">About Us</a>
                         </li>
                     </ul>
                 </div>
@@ -75,7 +50,6 @@
                     <a href="login" class="mx-2 btn" >Login</a>
                 </div>
             </div>
-
         </div>
     </nav>
     <!-- Close Header -->
@@ -100,75 +74,34 @@
 
 
     <!-- Start Banner Hero -->
+    {{-- <div id="template-mo-zay-hero-carousel" class="carousel slide" data-bs-ride="carousel">
+    <ol class="carousel-indicators">
+        <li data-bs-target="#template-mo-zay-hero-carousel" data-bs-slide-to="0" class="active"></li>
+        <li data-bs-target="#template-mo-zay-hero-carousel" data-bs-slide-to="1"></li>
+        <li data-bs-target="#template-mo-zay-hero-carousel" data-bs-slide-to="2"></li>
+    </ol> --}}
     <div id="template-mo-zay-hero-carousel" class="carousel slide" data-bs-ride="carousel">
-        <ol class="carousel-indicators">
-            <li data-bs-target="#template-mo-zay-hero-carousel" data-bs-slide-to="0" class="active"></li>
-            <li data-bs-target="#template-mo-zay-hero-carousel" data-bs-slide-to="1"></li>
-            <li data-bs-target="#template-mo-zay-hero-carousel" data-bs-slide-to="2"></li>
-        </ol>
         <div class="carousel-inner">
-            <div class="carousel-item active">
+            @foreach ($slider as $s => $sliders)
+            <div class="carousel-item {{$s == 0 ? 'active' : '' }}">
                 <div class="container">
                     <div class="row p-5">
                         <div class="mx-auto col-md-8 col-lg-6 order-lg-last">
-                            <img class="img-fluid" src="{{asset('vendor/img/banner_img_01.jpg')}}" alt="">
+                            <img class="img-fluid" src="{{ asset('storage/images/sliders/' . $sliders->image) }}" style="height: 30rem;" alt="">
                         </div>
                         <div class="col-lg-6 mb-0 d-flex align-items-center">
                             <div class="text-align-left align-self-center">
-                                <h1 class="h1 text-success"><b>Shop</b> eCommerce</h1>
-                                <h3 class="h2">Tiny and Perfect eCommerce Template</h3>
+                                {{-- <h1 class="h1 text-success"><b>Shop</b> eCommerce</h1>
+                                <h3 class="h2">Tiny and Perfect eCommerce Template</h3> --}}
                                 <p>
-                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                                    Sit quam in accusamus tempora dolore, neque adipisci nemo aut dignissimos 
-                                    eveniet.
+                                    {{ $sliders->description}}
                                 </p>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-            <div class="carousel-item">
-                <div class="container">
-                    <div class="row p-5">
-                        <div class="mx-auto col-md-8 col-lg-6 order-lg-last">
-                            <img class="img-fluid" src="{{asset('vendor/img/banner_img_02.jpg')}}" alt="">
-                        </div>
-                        <div class="col-lg-6 mb-0 d-flex align-items-center">
-                            <div class="text-align-left">
-                                <h1 class="h1">Proident occaecat</h1>
-                                <h3 class="h2">Aliquip ex ea commodo consequat</h3>
-                                <p>
-                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                                    Corrupti soluta sunt iste expedita provident nihil, 
-                                    fuga, perferendis officiis earum consequuntur accusamus 
-                                    fugiat ut quisquam ducimus totam itaque cupiditate saepe suscipit.
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="carousel-item">
-                <div class="container">
-                    <div class="row p-5">
-                        <div class="mx-auto col-md-8 col-lg-6 order-lg-last">
-                            <img class="img-fluid" src="{{asset('vendor/img/banner_img_03.jpg')}}" alt="">
-                        </div>
-                        <div class="col-lg-6 mb-0 d-flex align-items-center">
-                            <div class="text-align-left">
-                                <h1 class="h1">Repr in voluptate</h1>
-                                <h3 class="h2">Ullamco laboris nisi ut </h3>
-                                <p>
-                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                                    Corrupti soluta sunt iste expedita provident nihil, 
-                                    fuga, perferendis officiis earum consequuntur accusamus 
-                                    fugiat ut quisquam ducimus totam itaque cupiditate saepe suscipit.
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            @endforeach
         </div>
         <a class="carousel-control-prev text-decoration-none w-auto ps-3" href="#template-mo-zay-hero-carousel" role="button" data-bs-slide="prev">
             <i class="fas fa-chevron-left"></i>
@@ -176,12 +109,14 @@
         <a class="carousel-control-next text-decoration-none w-auto pe-3" href="#template-mo-zay-hero-carousel" role="button" data-bs-slide="next">
             <i class="fas fa-chevron-right"></i>
         </a>
-    </div>
+    </div>    
+    
+   
     <!-- End Banner Hero -->
 
     <!-- Start Featured Product -->
-    <section class="bg-light">
-        <div class="container py-5">
+    <section class="bg-light" id="product">
+        <div class="container py-5" >
             <div class="row text-center py-3">
                 <div class="col-lg-6 m-auto">
                     <h1 class="h1">Featured Product</h1>
@@ -190,79 +125,28 @@
                         Explicabo cupiditate cum autem quis dolorum totam maxime neque,
                 </div>
             </div>
-            <div class="row">
-                <div class="col-12 col-md-4 mb-4">
+            <div class="row" >
+                @foreach ($products as $p)
+                <div class="col-12 col-md-4 mb-4 ">
                     <div class="card h-100">
                         <a href="">
-                            <img src="{{asset('vendor/img/product_img_01.jpg')}}" class="card-img-top" alt="...">
+                            <img src="{{asset('storage/images/products/' . $p->image)}}" class="card-img" style="height: 21rem;" alt="...">
                         </a>
                         <div class="card-body">
                             <ul class="list-unstyled d-flex justify-content-between">
-                                <li>
-                                    <i class="text-warning fa fa-star"></i>
-                                    <i class="text-warning fa fa-star"></i>
-                                    <i class="text-warning fa fa-star"></i>
-                                    <i class="text-muted fa fa-star"></i>
-                                    <i class="text-muted fa fa-star"></i>
-                                </li>
-                                <li class="text-muted text-right">Rp.240.000</li>
+                                <li class="text-left">{{ $p->name_product}}</li>
+                                <li class="text-right">RP. {{number_format($p->price, 0)}}</li>
                             </ul>
-                            <a href="" class="h2 text-decoration-none text-dark">Casio Watch</a>
-                            <p class="card-text">
-                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt in culpa qui officia deserunt.
-                            </p>
-                            <p class="text-muted">Reviews (24)</p>
+                            @foreach ($categories as $item)
+                            <a class="h2 text-decoration-none text-dark">{{ $p->category_id == $item->id ? $item->name_category  : ''}}
+                            </a>
+                            @endforeach
+                            <p class="card-text">{{ $p->description}}</p>
+                            <a class="btn btn-success btn-lg">Pesan Sekarang !</a>
                         </div>
                     </div>
                 </div>
-                <div class="col-12 col-md-4 mb-4">
-                    <div class="card h-100">
-                        <a href="shop-single.html">
-                            <img src="{{asset('vendor/img/product_img_02.jpg')}}" class="card-img-top" alt="...">
-                        </a>
-                        <div class="card-body">
-                            <ul class="list-unstyled d-flex justify-content-between">
-                                <li>
-                                    <i class="text-warning fa fa-star"></i>
-                                    <i class="text-warning fa fa-star"></i>
-                                    <i class="text-warning fa fa-star"></i>
-                                    <i class="text-muted fa fa-star"></i>
-                                    <i class="text-muted fa fa-star"></i>
-                                </li>
-                                <li class="text-muted text-right">Rp.700.000</li>
-                            </ul>
-                            <a href="" class="h2 text-decoration-none text-dark">Cloud Nike Shoes</a>
-                            <p class="card-text">
-                                Aenean gravida dignissim finibus. Nullam ipsum diam, posuere vitae pharetra sed, commodo ullamcorper.
-                            </p>
-                            <p class="text-muted">Reviews (48)</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-12 col-md-4 mb-4">
-                    <div class="card h-100">
-                        <a href="shop-single.html">
-                            <img src="{{asset('vendor/img/product_img_03.jpg')}}" class="card-img-top" alt="...">
-                        </a>
-                        <div class="card-body">
-                            <ul class="list-unstyled d-flex justify-content-between">
-                                <li>
-                                    <i class="text-warning fa fa-star"></i>
-                                    <i class="text-warning fa fa-star"></i>
-                                    <i class="text-warning fa fa-star"></i>
-                                    <i class="text-warning fa fa-star"></i>
-                                    <i class="text-warning fa fa-star"></i>
-                                </li>
-                                <li class="text-muted text-right">Rp.650.000 </li>
-                            </ul>
-                            <a href="" class="h2 text-decoration-none text-dark">Summer Addides Shoes</a>
-                            <p class="card-text">
-                                Curabitur ac mi sit amet diam luctus porta. Phasellus pulvinar sagittis diam, et scelerisque ipsum lobortis nec.
-                            </p>
-                            <p class="text-muted">Reviews (74)</p>
-                        </div>
-                    </div>
-                </div>
+                @endforeach
             </div>
         </div>
     </section>
@@ -271,33 +155,34 @@
 
     <!-- Start Footer -->
     <footer class="bg-dark" id="tempaltemo_footer">
-        <div class="container">
+        <div class="container" id="about">
             <div class="row">
 
-                <div class="col-md-4 pt-5">
-                    <h2 class="h2 text-success border-bottom pb-3 border-light logo">Zay Shop</h2>
-                    <ul class="list-unstyled text-light footer-link-list">
-                        <li>
-                            <i class="fas fa-map-marker-alt fa-fw"></i>
-                            123 Consectetur at ligula 10660
-                        </li>
-                        <li>
-                            <i class="fa fa-phone fa-fw"></i>
-                            <a class="text-decoration-none" href="">0812384312</a>
-                        </li>
-                        <li>
-                            <i class="fa fa-envelope fa-fw"></i>
-                            <a class="text-decoration-none" href="">mycart@company.com</a>
-                        </li>
-                    </ul>
+                <div class="col-md-6 pt-5">
+                    <h2 class="text-success border-bottom pb-3 border-light logo">MyCart Shop</h2>
+                    <p class="text-light">MyCart merupakan platform toko online khusus untuk pria yang 
+                        menawarkan berbagai produk dengan fokus pada kebutuhan dan gaya hidup pria modern. 
+                        Kami menyediakan pengalaman belanja online yang nyaman bagi 
+                        para pelanggan kami.</p>
                 </div>
 
                 <div class="col-md-4 pt-5">
-                    <h2 class="h2 text-light border-bottom pb-3 border-light">Further Info</h2>
+                    <h2 class="h2 text-light border-bottom pb-3 border-light">Contact</h2>
                     <ul class="list-unstyled text-light footer-link-list">
-                        <li><a class="text-decoration-none" href="#">Home</a></li>
-                        <li><a class="text-decoration-none" href="#">About Us</a></li>
-                        <li><a class="text-decoration-none" href="#">Contact</a></li>
+                        <ul class="list-unstyled text-light footer-link-list">
+                            <li>
+                                <i class="fas fa-map-marker-alt fa-fw"></i>
+                                Jalan Mangga Besar III No. 17, Kab. Malang, Jawa Timur, 60256
+                            </li>
+                            <li>
+                                <i class="fa fa-phone fa-fw"></i>
+                                <a class="text-decoration-none" href="">0812384312</a>
+                            </li>
+                            <li>
+                                <i class="fa fa-envelope fa-fw"></i>
+                                <a class="text-decoration-none" href="">mycart@company.com</a>
+                            </li>
+                        </ul>
                     </ul>
                 </div>
 
